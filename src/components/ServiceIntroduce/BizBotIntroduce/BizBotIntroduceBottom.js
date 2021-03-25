2571677de94679e91cbb2ce23ba3a19bf9e43501
hyeseon <hyeseon8562@gmail.com>
2021-03-25 10:55:46 +0900

    :hammer: add bottom introduce file
    [ADD] - add bottom introduce files and class in BizBotIntroduce.scss

---
+++ src/components/ServiceIntroduce/BizBotIntroduce/BizBotIntroduceBottom.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './BizBotIntroduce.scss';
+
+const BizBotIntroduceBottom = () => {
+    return (
+        <div className='BizBotIntroduceBottom'>
+            <div className='BizBotIntroduceBottom__Top'></div>
+            <div className='BizBotIntroduceBottom__Bottom'></div>
+        </div>
+    )
+}
+
+export default BizBotIntroduceBottom;
