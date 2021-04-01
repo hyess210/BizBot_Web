0d8177e722a4793d463881043fcdaa4c85f2f244
hyeseon <hyeseon8562@gmail.com>
2021-04-01 16:40:33 +0900

    :dizzy: newSubscription animation
    [UPDATE] add bubble animation

--- src/components/NewsSubscription/NewsSubscription.js
+++ src/components/NewsSubscription/NewsSubscription.js
@@ -1,19 +1,32 @@
 import React from "react";
 import "./NewsSubscription.scss";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 import BUBBLE from "assets/images/bubble.png";
 
 const NewsSubscription = () => {
-    AOS.init();
+  AOS.init();
 
   return (
     <div className='NewsSubscription'>
       <div data-aos='zoom-in'>
         <img src={BUBBLE} className='NewsSubscription__bg' />
-      </div>
+      </div><div className='NewsSubscription__content'>
+          <div className='NewsSubscription__content-title'>
+            지원사업 카카오톡 뉴스레터 구독
+          </div>
+          <div className='NewsSubscription__content-des'>
+            인기 정부 지원 사업을 정리해 카카오톡 <br />
+            뉴스레터로 보내드립니다.
+          </div>
+          <hr className='NewsSubscription__content-line'/>
+          <div className='NewsSubscription__content-des' style={{fontWeight:'700'}}>
+          인기 정부 지원 사업을 정리해 카카오톡 <br/>
+뉴스레터로 보내드립니다.
+          </div>
+        </div>
     </div>
   );
 };
