258414e5a16a0643ec1d6da321d55438a770e27c
hyeseon <hyeseon8562@gmail.com>
2021-04-06 11:55:33 +0900

    :lipstick: design update
    [UPDATE] design update (background image delete, bg color change)

--- src/components/CountingUser/CountingUser.js
+++ src/components/CountingUser/CountingUser.js
@@ -9,19 +9,18 @@ const CountingUser = () => {
       <div className='CountingUser__Counting'>
         <div className='CountingUser__Counting-text'>
           지원사업 수(년)
-          <br />
-          <CountUp end={12000} className='CountingUser__Counting-number' />개
+          {' '}<CountUp end={12000} className='CountingUser__Counting-number' />{' '}개
         </div>
 
         <div className='CountingUser__Counting-text'>
-          누적 회원 수<br />{" "}
-          <CountUp end={213335} className='CountingUser__Counting-number' />
+          누적 회원 수
+          {' '}<CountUp end={213335} className='CountingUser__Counting-number' />{' '}
           명
         </div>
 
-        <div className='CountingUser__Counting-img-text'>
-          누적 검색 건 수<br />
-          <CountUp end={217396} className='CountingUser__Counting-number' />
+        <div className='CountingUser__Counting-text'>
+          누적 검색 건 수
+          {' '}<CountUp end={217396} className='CountingUser__Counting-number' />{' '}
           명
         </div>
       </div>
