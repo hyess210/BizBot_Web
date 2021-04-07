7e206d94b555d223a645aac6a4c52a1a08695fdc
hyeseon <hyeseon8562@gmail.com>
2021-04-07 20:28:57 +0900

    :lipstick: InMedia ,Main layout
    [UPDATE] InMedia design, main page layout changed

--- src/components/InMedia/InMedia.js
+++ src/components/InMedia/InMedia.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./InMedia.scss";
 
 import MEDIA01 from "assets/images/media1.png";
-import { FiChevronsRight } from "react-icons/fi";
+// import { FiChevronsRight } from "react-icons/fi";
+import { RiArrowDropRightFill, RiArrowDropLeftFill } from "react-icons/ri";
 
 const InMedia = () => {
   const InMediaItems = ({ title, content, url, imageSrc }) => {
@@ -10,10 +11,10 @@ const InMedia = () => {
       <li className='InMedia__List-Items'>
         <a href={url} target='_blank' className='InMedia__List-Items-box'>
           <img src={imageSrc} alt={imageSrc} />
-          <div className='InMedia__List-Items-title'>
+          {/* <div className='InMedia__List-Items-title'>
             {title}
             <FiChevronsRight className='InMedia__List-Items-icon' />
-          </div>
+          </div> */}
           <div className='InMedia__List-Items-content'>{content}</div>
         </a>
       </li>
@@ -23,8 +24,10 @@ const InMedia = () => {
     <div className='InMedia'>
       <div className='InMedia__Header'>
         <div>언론 속의 비즈봇</div>
-        <div className='InMedia__Header-des'>언론 속에 비친 비즈봇입니다.</div>
       </div>
+      <button className='InMedia__icon-left'>
+        <RiArrowDropLeftFill />
+      </button>
       <ul className='InMedia__List'>
         <InMediaItems
           title='언론사'
@@ -45,6 +48,8 @@ const InMedia = () => {
           imageSrc={MEDIA01}
         />
       </ul>
+      <button className='InMedia__icon-right'>
+      <RiArrowDropRightFill /></button>
     </div>
   );
 };
