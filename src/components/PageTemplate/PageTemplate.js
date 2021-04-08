ce1254f7fe2dc27aa319020b62779b829bdb8554
hyeseon <hyeseon8562@gmail.com>
2021-04-08 11:44:51 +0900

    :hammer: :lipstick: add scroll top button
    [ADD] scroll top button
    [UPDATE] footer style

--- src/components/PageTemplate/PageTemplate.js
+++ src/components/PageTemplate/PageTemplate.js
@@ -1,5 +1,6 @@
 import NavBar from "components/Common/NavBar/NavBar";
 import Footer from "components/Common/Footer/Footer";
+import ScrollTop from 'components/Common/ScrollTop/ScrollTop';
 import React from "react";
 
 const PageTemplate = ({ children }) => {
@@ -8,6 +9,7 @@ const PageTemplate = ({ children }) => {
       <NavBar />
       {children}
       <Footer />
+      <ScrollTop/>
     </div>
   );
 };
