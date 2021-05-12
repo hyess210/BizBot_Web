b592cb39a17d832d79cdc0ed0eba099c0907e6c8
hyeseon <hyeseon8562@gmail.com>
2021-05-12 15:52:27 +0900

    :lipstick: article link update
    [UPDATE] update article link and remove arrow buttons

--- src/components/Main/InMedia/InMedia.js
+++ src/components/Main/InMedia/InMedia.js
@@ -25,31 +25,31 @@ const InMedia = () => {
       <div className='InMedia__Header'>
         <div>언론 속의 비즈봇</div>
       </div>
-      <button className='InMedia__icon-left'>
+      {/* <button className='InMedia__icon-left'>
         <RiArrowDropLeftFill />
-      </button>
+      </button> */}
       <ul className='InMedia__List'>
         <InMediaItems
           title='언론사'
-          content='기사 제목 기사 제목 기사 제목 기사 제목 기사 제목 기사 제목 기사 제목 기사 제목'
-          url='#'
+          content='대구시-스타트업 페르소나 기업 지원 위한 업무협약 체결'
+          url='http://www.w-startup.com/news/articleView.html?idxno=11277'
           imageSrc={MEDIA01}
         />
         <InMediaItems
           title='언론사'
-          content='기사 제목 기사 제목 기사 제목 기사 제목 기사 제목 기사 제목 기사 제목 기사 제목'
-          url='#'
+          content='중소기업연구원, 페르소나와 ‘정책정보 제공서비스 업무협약'
+          url='https://www.shinailbo.co.kr/news/articleView.html?idxno=1345898'
           imageSrc={MEDIA01}
         />
         <InMediaItems
           title='언론사'
-          content='기사 제목 기사 제목 기사 제목 기사 제목 기사 제목 기사 제목 기사 제목 기사 제목'
-          url='#'
+          content='페르소나, 대구市 공공데이터 활용 창업경진대회 최우수상 수상'
+          url='https://m.wowtv.co.kr/NewsCenter/News/Read?articleId=A202008210326#_enliple'
           imageSrc={MEDIA01}
         />
       </ul>
-      <button className='InMedia__icon-right'>
-      <RiArrowDropRightFill /></button>
+      {/* <button className='InMedia__icon-right'>
+      <RiArrowDropRightFill /></button> */}
     </div>
   );
 };
