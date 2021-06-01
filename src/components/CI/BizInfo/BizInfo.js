d9131ed4050a09d8e77cfc218e40d10d7060b61e
hyeseon <hyeseon8562@gmail.com>
2021-06-01 15:58:44 +0900

    :lipstick: CI slogan, brand
    [UPDATE] 회사 소개 내용 추가 및 레이아웃 스타일링

--- src/components/CI/BizInfo/BizInfo.js
+++ src/components/CI/BizInfo/BizInfo.js
@@ -19,9 +19,28 @@ const BizInfo = () => {
   return (
     <div className='BizInfo'>
       <ContentTemplate title='SLOGAN'>
-        민관공 각각의 ‘페르소나’를 존중한 고민과 소통을 시작합니다. <br />
-        시대의 흐름에 맞는 디지털 혁신(빅데이터, AI 등)을 기반한 포용으로써,<br />
-        민관공의 관계를 존중하고 연결하는 국내 최대 플랫폼을 만들어 갑니다.
+        <b>민관공 각각의 ‘페르소나’를 존중한 고민과 소통을 시작</b>합니다. <br />
+        시대의 흐름에 맞는 <b>디지털 혁신(빅데이터, AI 등)을 기반한 포용</b>으로써,<br />
+        <b>민관공의 관계를 존중하고 연결하는 국내 최대 플랫폼</b>을 만들어 갑니다.
+      </ContentTemplate>
+      <ContentTemplate title='BRAND'>
+        <div className='BizInfo__content-children-brand'>
+          <b>Vision</b><br/>
+          국내 최대 플랫폼 제작
+        </div><div className='BizInfo__content-children-brand'>
+          <b>Mission</b><br/>
+          디지털 혁신 기반 민관공 연결
+        </div><div className='BizInfo__content-children-brand'>
+          <b>Positioning</b><br/>
+          디지털 혁신을 통한 민관공을 쉽고<br/>
+          간편하게 연결하는 플랫폼 제작사
+        </div><div className='BizInfo__content-children-brand'>
+          <b>Personality</b><br/>
+          존중, 소통, 포용
+        </div><div className='BizInfo__content-children-brand'>
+          <b>Core Value</b><br/>
+          시대의 흐름에 맞는 디지털 혁신
+        </div>
       </ContentTemplate>
     </div>
   );
