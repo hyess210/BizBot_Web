7552d52301ca75a12da67f9d0b0635e4892bf0a8
hyeseon <hyeseon8562@gmail.com>
2021-06-09 16:07:21 +0900

    :dizzy: add animation
    [UPDATE] add flip animation

--- src/components/Main/SearchCategory/SearchCategory.js
+++ src/components/Main/SearchCategory/SearchCategory.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./SearchCategory.scss";
 
+import "aos/dist/aos.css";
+
 import CATEGORY_IMG from "assets/images/illustrator_img.png";
 
 const SearchCategory = () => {
   return (
     <div className='SearchCategory'>
+      <div data-aos='flip-up'>
       <div className='SearchCategory__content'>
         <h1 className='SearchCategory__content-title'>
           전국 약 500여개 중앙부처, 지자체, 공공기관의
@@ -16,8 +19,11 @@ const SearchCategory = () => {
             가장 빠르고 쉬운 비즈봇 서비스에 이미 22만 중소기업이 함께하고 있습니다.
         </div>
       </div>
+      </div>
+      <div data-aos='flip-up'>
       <div className='SearchCategory__img'>
         <img src={CATEGORY_IMG} alt={CATEGORY_IMG} />
+      </div>
       </div>
     </div>
   );
